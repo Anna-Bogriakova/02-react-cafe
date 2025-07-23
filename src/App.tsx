2dd9f0cf428bfdc029c2c9dcc8ae7bdaf4dcd609
Anna Bogriakova <anna.bogriakova@gmail.com>
2025-07-24 00:39:26 +0200

    homework

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import { Votes, VoteType } from "../../types/votes";
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  return (
+    <div className={styles.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      <VoteStats votes={votes} />
+    </div>
+  );
+}
+
+export default App;
